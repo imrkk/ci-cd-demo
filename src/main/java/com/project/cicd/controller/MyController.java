b45a0c5aeab8c9dcf4409b3c321cc1a87bf9e5dd
Ravi <itsrkjii@gmail.com>
2024-01-06 21:04:10 +0530

    first commit

---
+++ src/main/java/com/project/cicd/controller/MyController.java
@@ -0,0 +1,25 @@
+package com.project.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+public class MyController {
+	
+	@PostMapping("/getData")
+	public String getUserData() {
+		return "Hello Ravi";
+	}
+	
+	
+	@GetMapping("/getData")
+	public String userData() {
+		return "Hello Ravi Kumar";
+	}
+	
+	
+
+}
